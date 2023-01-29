c9df2b96faf4fbe4ebe4baf1227c97592a23e388
Deveesh Shetty <deveeshshetty@gmail.com>
2023-01-29 18:43:31 +0530

    Initial dist subtree commit

--- frontend/src/context/LangaugageModalContext.ts
+++
@@ -1,8 +0,0 @@
-import React from "react"
-type LangaugeProps = {
-  languageModal: boolean
-  setLanguageModal: any
-}
-const LanguageModalContext = React.createContext<Partial<LangaugeProps>>({})
-
-export default LanguageModalContext
